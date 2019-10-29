8d8ffebc800f3d381c6cc2c5ecf3e3f64f548e97
bruna-capistrano <54087350+bruna-capistrano@users.noreply.github.com>
2019-10-29 12:30:30 -0300

    remove comments

--- ArvoreB - Indexar CEPs (Trabalho3)/ArvoreB.h
+++ ArvoreB - Indexar CEPs (Trabalho3)/ArvoreB.h
@@ -1,8 +1,8 @@
-#ifndef __ARVORE_B__ //se não estiver definido
-#define __ARVORE_B__//defino
-//tudo que começa com # é diretiva de pré-processador
+#ifndef __ARVORE_B__ 
+#define __ARVORE_B__
+
 #define TAM_CHAVE 8
-#define TAM_PAG 300 //arvore com 300 chaves
+#define TAM_PAG 300 
 
 #include <stdio.h>
 
@@ -10,12 +10,12 @@ typedef struct _ArvoreB ArvoreB;
 typedef struct _ArvoreB_Cabecalho ArvoreB_Cabecalho;
 typedef struct _ArvoreB_Elemento ArvoreB_Elemento;
 typedef struct _ArvoreB_Pagina ArvoreB_Pagina;
-//pre def das estruturas que vou usar
+
 
 
 struct _ArvoreB_Cabecalho
 {
-    long raiz;//ponteiro para a raiz. Como vou trabalhar em disco, é a posição absoluta da raiz no arquivo, parametro do fseek
+    long raiz;
 };
 
 struct _ArvoreB
@@ -26,16 +26,16 @@ struct _ArvoreB
 
 struct _ArvoreB_Elemento
 {
-    char chave[TAM_CHAVE];//um elemento tem uma chave e o ponteiro para o arquivo de dados
-    long posicaoRegistro;//posicao do registro no arquivo de dados
-    long paginaDireita;//posição da página da direita, então toda chave conhece a página da direita
+    char chave[TAM_CHAVE];
+    long posicaoRegistro;
+    long paginaDireita;
 };
 
 struct _ArvoreB_Pagina
 {
-    int quantidadeElementos;//qtd de elem. armazenados na pagina
-    long paginaEsquerda;//ponteiro da esquerda da página
-    ArvoreB_Elemento elementos[TAM_PAG];//vetor que armazena elementos da página, tem 300 elementos
+    int quantidadeElementos;
+    long paginaEsquerda;
+    ArvoreB_Elemento elementos[TAM_PAG];
 };
 
 #endif
